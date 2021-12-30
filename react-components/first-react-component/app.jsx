628e160d94c0e9bb5b97bcec856176397790d0b3
Jurytan Nguyen <jnguyen7410@gmail.com>
2021-12-29 23:58:17 -0500

    Adding the first half of React Components

---
+++ react-components/first-react-component/app.jsx
@@ -0,0 +1,29 @@
+// 01
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+class MyComponentClass extends React.Component {
+  render() {
+    return <h1>Hello world</h1>;
+  }
+};
+ 
+ReactDOM.render(
+  <MyComponentClass />,
+  document.getElementById('app')
+);
+
+// 02
+import React from 'react';
+
+// 03
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// 04
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class x extends React.Component {
+
+}
