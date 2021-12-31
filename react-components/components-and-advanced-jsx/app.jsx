b8f6806074304356614ebc597656918b7b1ac1b9
Jurytan Nguyen <jnguyen7410@gmail.com>
2021-12-31 03:08:53 -0500

    Adding the rest of components and advanced jsx

--- react-components/components-and-advanced-jsx/app.jsx
+++ react-components/components-and-advanced-jsx/app.jsx
@@ -34,5 +34,101 @@ const owl = {
 
 // Component class starts here:
 class Owl extends React.Component {
-  
+  render() {
+    return (<div>
+      <h1>{owl.title}</h1>
+      <img
+        src={owl.src}
+        alt={owl.title}
+      />
+    </div>);
+  }
 }
+
+ReactDOM.render(<Owl />, document.getElementById('app'));
+
+// 03
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const friends = [
+  {
+    title: "Yummmmmmm",
+    src: "https://content.codecademy.com/courses/React/react_photo-monkeyweirdo.jpg"
+  },
+  {
+    title: "Hey Guys!  Wait Up!",
+    src: "https://content.codecademy.com/courses/React/react_photo-earnestfrog.jpg"
+  },
+  {
+    title: "Yikes",
+    src: "https://content.codecademy.com/courses/React/react_photo-alpaca.jpg"
+  }
+];
+
+// New component class starts here:
+class Friend extends React.Component {
+  render() {
+    let friend = friends[2]; 
+    return (
+      <div>
+        <h1>{friend.title}</h1>
+        <img src={friend.src} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Friend />, document.getElementById('app'));
+
+
+// 04
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const fiftyFifty = Math.random() < 0.5;
+
+// New component class starts here:
+class TonightsPlan extends React.Component {
+  render() {
+    let text = fiftyFifty ? 'Tonight I\'m going out WOOO' : 'Tonight I\'m going to bed WOOO';
+
+    return <h1>{text}</h1>
+  }
+}
+
+ReactDOM.render(<TonightsPlan />, document.getElementById('app'));
+
+// 05
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class MyName extends React.Component {
+	// name property goes here:
+get name() {
+  return 'Jurytan';
+}
+
+  render() {
+    return <h1>My name is {this.name}.</h1>;
+  }
+}
+
+ReactDOM.render(<MyName />, document.getElementById('app'));
+
+// 06
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Button extends React.Component {
+  scream() {
+    alert('AAAAAAAAHHH!!!!!');
+  }
+
+  render() {
+    return <button onClick={this.scream}>AAAAAH!</button>;
+  }
+}
+
+ReactDOM.render(<Button />, document.getElementById('app'));
