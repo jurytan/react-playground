edac8e2c8efe34d4a2e5b95694e3097349ad7930
jnguyen7410 <jurytann@grubhub.com>
2022-01-14 03:20:32 -0500

    Adding another part of the state hook section

---
+++ hooks/the-state-hook/GroceryCart.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import ItemList from "./ItemList";
+import { produce, pantryItems } from "./storeItems";
+
+export default function GroceryCart() {
+  // declare and initialize state 
+  const [cart, setCart] = useState([]);
+
+  const addItem = (item) => {
+    setCart((prev) => [item, ...prev]);
+   };
+
+  const removeItem = (targetIndex) => {
+    setCart((prev) => prev.filter((_, i) => i !== targetIndex));
+  };
+
+  return (
+    <div>
+      <h1>Grocery Cart</h1>
+      <ul>
+        {cart.map((item, index) => (
+          <li onClick={() => removeItem(index)} key={index}>
+            {item}
+          </li>
+        ))}
+      </ul>
+      <h2>Produce</h2>
+      <ItemList items={produce} onItemClick={addItem} />
+      <h2>Pantry Items</h2>
+      <ItemList items={pantryItems} onItemClick={addItem} />
+    </div>
+  );
+}
