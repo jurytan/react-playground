c04421e03fee9ceb975ac239d9ec374432b0f553
jnguyen7410 <jurytann@grubhub.com>
2022-01-25 00:21:58 -0500

    Finishing up the example

--- stateless-components-from-stateful-components/child-components-update-their-parents-state/Child.jsx
+++ stateless-components-from-stateful-components/child-components-update-their-parents-state/Child.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
 export class Child extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(e) {
+    const name = e.target.value;
+    this.props.onChange(name);
+  }
   render() {
     return (
       <div>
         <h1>
           Hey my name is {this.props.name}!
         </h1>
-        <select id="great-names">
+        <select id="great-names" onChange={this.handleChange}>
           <option value="Frarthur">
             Frarthur
           </option>
